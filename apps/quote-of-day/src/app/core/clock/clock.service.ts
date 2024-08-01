cdf25ea7636268093dfda319f83f1f7947064588
Pratik Sharma <pratiksharma@live.in>
2024-08-01 13:51:09 +0200

    Clock fixed

--- apps/quote-of-day/src/app/core/clock/clock.service.ts
+++ apps/quote-of-day/src/app/core/clock/clock.service.ts
@@ -19,20 +19,23 @@ export class ClockService {
     //date object
     const date = new Date();
 
-    var h = date.getHours();
-    var m = date.getMinutes();
-    var s = date.getSeconds();
-    var session = 'AM';
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = 'AM';
 
-    if (h == 0) {
-      h = 12;
-    }
-    if (h > 12) {
-      h = h - 12;      
-    }
-    if (h > 11) {
+    if(h > 12) {
       session = 'PM';
     }
+    // if (h == 0) {
+    //   h = 12;
+    // }
+    // if (h > 12) {
+    //   h = h - 12;      
+    // }
+    // if (h > 11) {
+    //   session = 'PM';
+    // }
 
     //small tag para los segundos y el pm/am
     const time = `${h < 10 ? '0' : ''}${h}:${m < 10 ? '0' : ''}${m}:<small>${
@@ -41,16 +44,27 @@ export class ClockService {
 
     let text;
 
-    if (h >= 5 && h <= 8 && session === 'AM') {
-      text = `Good Morning`;
-    } else if (h > 8 && h < 12 && session === 'AM') {
-      text = `Good Day`;
-    } else if (h >= 1 && h <= 5 && session === 'PM') {
-      text = `Good Afternoon`;
-    } else if (h <= 10 && session === 'PM') {
-      text = `Good Evening`;
-    } else {
-      text = `Good Night`;
+    // if (h >= 5 && h <= 8 && session === 'AM') {
+    //   text = `Good Morning`;
+    // } else if (h > 8 && h < 12 && session === 'AM') {
+    //   text = `Good Day`;
+    // } else if (h >= 1 && h <= 5 && session === 'PM') {
+    //   text = `Good Afternoon`;
+    // } else if (h <= 10 && session === 'PM') {
+    //   text = `Good Evening`;
+    // } else {
+    //   text = `Good Night`;
+    // }
+    if (h >= 5 && h <= 9) {
+        text = `Good Morning`;
+      } else if (h > 9 && h < 12) {
+        text = `Good Day`;
+      } else if (h >= 13 && h <= 5) {
+        text = `Good Afternoon`;
+      } else if (h <= 22) {
+        text = `Good Evening`;
+      } else {
+        text = `Good Night`;
     }
 
     if (!this.clock$) return;
